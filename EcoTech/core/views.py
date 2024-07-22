c8aa6d768470e5fb8ee6e17b65a242c58e03af4b
Nana <nanasmacbookprowithtouchbar@songruohandeMacBook-Pro.local>
2024-07-22 00:40:08 -0400

    Ruohan | issue 17 | history count numbers added on home page

--- EcoTech/core/views.py
+++ EcoTech/core/views.py
@@ -3,19 +3,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
-from contents.models import Article
+from contents.models import Article, Comment
+from users.models import Member
 
 
 # Home page view- will load the default landing page
 def home_view(request):
     daily_visits = request.COOKIES.get('daily_visits', 0)
-    # total_visits = request.COOKIES.get('total_visits', 0)
+    total_visits = request.COOKIES.get('total_visits', 0)
 
     num_article = Article.objects.count()
+    num_member = Member.objects.count()
+    num_comment = Comment.objects.count()
+
     context = {
         'daily_visits': daily_visits,
-        # 'total_visits': total_visits,
+        'num_comment': num_comment,
         'num_article': num_article,
+        'num_member': num_member,
     }
 
     return render(request, 'core/home.html', context)
--- EcoTech/core/visit_middleware.py
+++ EcoTech/core/visit_middleware.py
@@ -10,8 +10,8 @@ class VisitMiddleware(MiddlewareMixin):
 
         if 'visit_counted' not in request.session:
             request.session['visit_counted'] = True
-            request.session['total_visits'] = request.session.get('total_visits', 0) + 1
-            print(f"New session visit counted: total_visits={request.session['total_visits']}")
+            # request.session['total_visits'] = request.session.get('total_visits', 0) + 1
+            # print(f"New session visit counted: total_visits={request.session['total_visits']}")
 
         # Initialize daily visits counter based on cookies
         last_visit_date = request.COOKIES.get('last_visit_date')
@@ -22,11 +22,11 @@ class VisitMiddleware(MiddlewareMixin):
 
         request.session['last_visit_date'] = current_date
 
-        print(f"Session visits updated: total_visits={request.session['total_visits']}, daily_visits={request.session['daily_visits']}")
+        # print(f"Session visits updated: total_visits={request.session['total_visits']}, daily_visits={request.session['daily_visits']}")
 
     def process_response(self, request, response):
         # Set cookies for last visit date, daily visits, and total visits
         response.set_cookie('last_visit_date', request.session.get('last_visit_date'))
         response.set_cookie('daily_visits', request.session.get('daily_visits', 0))
-        response.set_cookie('total_visits', request.session.get('total_visits', 0))
+        # response.set_cookie('total_visits', request.session.get('total_visits', 0))
         return response
