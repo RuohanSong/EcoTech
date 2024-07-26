84952da2cdd68f06499203f98a1d57f51b4173f1
sadiaanzum <anzum.sadia@gmail.com>
2024-07-25 22:32:52 -0400

    added edit and delete article feature

--- EcoTech/contents/urls.py
+++ EcoTech/contents/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
     path('search/', views.article_search, name='article_search'),
     path('articles/<int:pk>/', views.article_detail, name='article_detail'),
+    path('article/<int:pk>/edit/', views.edit_article, name='edit_article'),
+    path('article/<int:pk>/delete/', views.delete_article, name='delete_article'),
+    path('article/<int:pk>/confirm_delete/', views.confirm_delete_article, name='confirm_delete_article'),
 ]
